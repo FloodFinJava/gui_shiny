79718c901e1f66898d51c2302a36e79189ddacc8
Laurent Courty <lrntct@gmail.com>
2018-05-17 09:05:37 +0100

    add dropdown

--- src/app.R
+++ src/app.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(leaflet)
-library(sp)
 library(rgdal)
 library(raster)
 
@@ -8,22 +7,24 @@ library(raster)
 loss_map <- readOGR("/home/laurent/Dropbox/FloodFinJava/geodata/central_java_hospitals_losses.gpkg")
 flood_map <- raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif")
 
-color_palette <-  colorNumeric(palette = "GnBu", domain = values(flood_map), na.color=NA)
+color_palette <-  colorNumeric(palette = "YlGnBu", domain = values(flood_map), na.color=NA)
 
 # UI
 ui <- fluidPage(
   titlePanel("A flood loss example"),
   sidebarLayout(position = "left",
-    sidebarPanel("The other stuffs"),
+    sidebarPanel("The other stuffs",
+                 selectInput(inputId = "return_period", label = "Return period",
+                             choices = c(200,1500))),
     mainPanel("The MAP",
-              leafletOutput("mymap")
+              leafletOutput("map_area")
               )
     )
   )
 
 # Server
 server <- function(input, output) {
-  output$mymap <- renderLeaflet({
+  output$map_area <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       addPolygons(data = loss_map) %>%
