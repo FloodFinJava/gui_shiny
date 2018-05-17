3000feaf9653828ba6c9df2435832d09a9f23aa6
Laurent Courty <lrntct@gmail.com>
2018-05-17 13:00:41 +0100

    put maps in a names list

--- src/app.R
+++ src/app.R
@@ -5,10 +5,9 @@ library(raster)
 
 # Maps
 loss_map <- readOGR("/home/laurent/Dropbox/FloodFinJava/geodata/central_java_hospitals_losses.gpkg")
-flood_maps <- c("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif",
-                "/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q1500_RD_031.tif")
-return_periods <- c(200,1500)
-names(flood_maps) <- return_periods
+flood_maps <- list('200'=raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif"),
+                   '1500'=raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q1500_RD_031.tif"))
+return_periods <- names(flood_maps)
 
 color_palette <-  colorNumeric(palette = "YlGnBu", domain = c(0,30), na.color=NA)
 
@@ -31,8 +30,8 @@ server <- function(input, output) {
     leaflet() %>%
       addTiles() %>%
       addPolygons(data = loss_map) %>%
-      addRasterImage(raster(flood_maps[input$return_period]), colors = color_palette, opacity = 0.8) %>%
-      addLegend(pal = color_palette, values = values(raster(flood_maps[input$return_period])), title = "Max. water depth")
+      addRasterImage(flood_maps[[input$return_period]], colors = color_palette, opacity = 0.8) %>%
+      addLegend(pal = color_palette, values = values(flood_maps[[input$return_period]]), title = "Max. water depth")
       })
 }
 
