997afc020c49f17690ad89d8bf6c32c5b5eb9369
Laurent Courty <lrntct@gmail.com>
2018-05-17 10:39:35 +0100

    dropdown selects map

--- src/app.R
+++ src/app.R
@@ -5,18 +5,21 @@ library(raster)
 
 # Maps
 loss_map <- readOGR("/home/laurent/Dropbox/FloodFinJava/geodata/central_java_hospitals_losses.gpkg")
-flood_map <- raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif")
+flood_maps <- c("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif",
+                "/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q1500_RD_031.tif")
+return_periods <- c(200,1500)
+names(flood_maps) <- return_periods
 
-color_palette <-  colorNumeric(palette = "YlGnBu", domain = values(flood_map), na.color=NA)
+color_palette <-  colorNumeric(palette = "YlGnBu", domain = c(0,30), na.color=NA)
 
 # UI
 ui <- fluidPage(
   titlePanel("A flood loss example"),
   sidebarLayout(position = "left",
-    sidebarPanel("The other stuffs",
-                 selectInput(inputId = "return_period", label = "Return period",
-                             choices = c(200,1500))),
-    mainPanel("The MAP",
+    sidebarPanel("",
+                 selectInput(inputId = "in_return_period", label = "Return period",
+                             choices = return_periods)),
+    mainPanel("",
               leafletOutput("map_area")
               )
     )
@@ -28,8 +31,8 @@ server <- function(input, output) {
     leaflet() %>%
       addTiles() %>%
       addPolygons(data = loss_map) %>%
-      addRasterImage(flood_map, colors = color_palette, opacity = 0.8) %>%
-      addLegend(pal = color_palette, values = values(flood_map), title = "Max. water depth")
+      addRasterImage(raster(flood_maps[input$in_return_period]), colors = color_palette, opacity = 0.8) %>%
+      addLegend(pal = color_palette, values = values(raster(flood_maps[input$in_return_period])), title = "Max. water depth")
       })
 }
 
