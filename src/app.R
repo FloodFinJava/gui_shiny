d67c83fed3d600b65dc646c824918c0383cdc784
Laurent Courty <lrntct@gmail.com>
2018-06-13 11:08:39 +0100

    display losses in rupiah. Change color scheme

--- src/app.R
+++ src/app.R
@@ -10,7 +10,7 @@ loss_map <- readOGR("/home/laurent/Dropbox/FloodFinJava/geodata/central_java_hos
 flood_maps <- list('200'=raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q200_RD_031.tif"),
                    '1500'=raster("/home/laurent/Dropbox/FloodFinJava/geodata/JBA/Sample data/Semarang/ID_FLRF_UD_Q1500_RD_031.tif"))
 return_periods <- names(flood_maps)
-color_palette <-  colorNumeric(palette = "YlGnBu", domain = values(flood_maps[['1500']]), na.color=NA)
+color_palette <-  colorNumeric(palette = "Blues", domain = values(flood_maps[['1500']]), na.color=NA)
 
 # UI
 ui <- fluidPage(
@@ -22,10 +22,11 @@ ui <- fluidPage(
                  verbatimTextOutput("summary")
                  ),
     mainPanel("",
-              leafletOutput("map_area")
+              leafletOutput("map_area", height = 800)
               )
     )
   )
+
 
 # Server
 server <- function(input, output) {
@@ -34,9 +35,11 @@ server <- function(input, output) {
   perc_losses <- reactive({sprintf("q%s_perc_losses", input$return_period)})
   value_loss <- reactive({sprintf("q%s_losses", input$return_period)})
   popup_content <- reactive({paste0("<b>", htmlEscape(loss_map$name), "</b>",
-                          sprintf("<br> Value: $%1.0f", loss_map$asset_value),
+                          sprintf("<br> Value (MRp): %s", prettyNum(loss_map$asset_value,
+                                                                    big.mark=" ", scientific=F)),
                           "<br> Losses: ", percent(loss_map[[perc_losses()]]),
-                          sprintf("<br> Value loss: $%1.0f", loss_map[[value_loss()]])
+                          sprintf("<br> Value loss (MRp): %s", format(loss_map[[value_loss()]],
+                                                                      big.mark=" ", scientific=F))
                           )
                           })
 
@@ -47,7 +50,8 @@ server <- function(input, output) {
   output$summary <- renderText({
     paste(
       sprintf("Number of flooded assets: %1.0f", sum(loss_map[[perc_losses()]] != 0)),
-      sprintf("Total financial losses: $%1.0f", sum(loss_map[[value_loss()]])),
+      sprintf("Total financial losses (MRp): %s", format(sum(loss_map[[value_loss()]]),
+                                                         big.mark=" ", scientific=F)),
       sep = '\n'
     )
   })
@@ -65,7 +69,7 @@ server <- function(input, output) {
   observe({
     leafletProxy("map_area") %>%
       clearShapes() %>%
-      addPolygons(data = loss_map, popup = popup_content())
+      addPolygons(data = loss_map, popup = popup_content(), color = "#4daf4a")
   })
   # draw/update flood map
   observe({
